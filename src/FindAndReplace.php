4b4568d372955dce5fc9629d65663f23ad95955d
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-11 14:07:32 -0700

    Add replace method for replacing with different as input

--- src/FindAndReplace.php
+++ src/FindAndReplace.php
@@ -4,6 +4,9 @@
     {
         function replace($user_string, $thing_to_replace, $what_to_replace_with)
         {
+            if($thing_to_replace == $user_string){
+                $user_string = $what_to_replace_with;
+            }
             return $user_string;
         }
     }
