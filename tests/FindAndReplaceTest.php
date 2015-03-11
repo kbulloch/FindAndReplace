5ac3960942c9940ff317617ca5bfd05f8f4c853c
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-11 13:57:20 -0700

    Add test for replace with same letter

---
+++ tests/FindAndReplaceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+    class FindAndReplaceTest extends PHPUnit_Framework_TestCase
+    {
+        function testReplaceWithSameLetter()
+        {
+            //Arrange
+            $replace_with_same_letter = new FindAndReplace;
+            $main_string = "a";
+            $thing_to_find = "a";
+            $replace_with = "a";
+
+            //Act
+            $result = $replace_with_same_letter->replace($main_string, $thing_to_find, $replace_with);
+
+            //Assert
+            $this->assertEquals("a", $result);
+        }
+    }
+
+?>
